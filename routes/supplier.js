6dbfd8e0cb68f8f9541a6e89376445aec5c5a53a
Alfredo López <alfredolopezavelar1@gmail.com>
2025-04-27 12:57:40 -0600

    Added a route for handling the users

--- routes/supplier.js
+++ routes/supplier.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const Supplier = require("../models/supplier.model")
-const SupplierController = require("../controllers/SupplierController")
+const ProviderController = require("../controllers/ProviderController")
 
-router.post('/', SupplierController.createSupplier);
-router.get('/', SupplierController.getSuppliers);
+router.post('/', ProviderController.createProvider);
+router.get('/', ProviderController.getSuppliers);
 
 module.exports = router;
